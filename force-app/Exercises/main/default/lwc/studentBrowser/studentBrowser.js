ef372c23a194795d8b254395c63f92ce3a2f5435
Swapnil Borkar <swapnil.borkar@salesforce.com>
2021-04-23 12:10:44 -0400

    Exercise 6-5 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,72 +1,77 @@
-import { LightningElement ,wire} from 'lwc';
-import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
-import { publish, MessageContext } from 'lightning/messageService';
-import SELECTED_STUDENT_CHANNEL from '@salesforce/messageChannel/SelectedStudentChannel__c';
-import { NavigationMixin } from 'lightning/navigation';
+import { LightningElement, wire } from "lwc";
+import getStudents from "@salesforce/apex/StudentBrowser.getStudents";
+import { publish, MessageContext } from "lightning/messageService";
+import SELECTED_STUDENT_CHANNEL from "@salesforce/messageChannel/SelectedStudentChannel__c";
+import { NavigationMixin } from "lightning/navigation";
 
 export default class StudentBrowser extends NavigationMixin(LightningElement) {
-    selectedDeliveryId = '';
-    selectedInstructorId = '';
-    @wire(getStudents, {instructorId:'$selectedInstructorId',courseDeliveryId :'$selectedDeliveryId'}) 
-    students;
-    @wire(MessageContext) messageContext;
-    cols = [
-        {
-        fieldName:"Name",
-        label: "Name"
-        },
-        {
-        fieldName:"Title",
-        label: "Title",
-        hiddenOnMobile: true
-        },
-        {
-        fieldName:"Phone",
-        label: "Phone",
-        type: "phone"
-        },
-        {
-        fieldName:"Email",
-        label: "E-Mail",
-        type: "email"
-        }
-        ];
-  
-    handleFilterChange(event){
-        this.selectedDeliveryId = event.detail.deliveryId;
-        this.selectedInstructorId = event.detail.instructorId;
-        }
+	selectedDeliveryId = "";
+	selectedInstructorId = "";
+	students = [];
+	@wire(getStudents, { instructorId: "$selectedInstructorId", courseDeliveryId: "$selectedDeliveryId" })
+	wired_getStudents(result) {
+		this.students = result;
+		this.dispatchEvent(new CustomEvent("doneloading", { bubbles: true, composed: true }));
+	}
+	@wire(MessageContext) messageContext;
+	cols = [
+		{
+			fieldName: "Name",
+			label: "Name"
+		},
+		{
+			fieldName: "Title",
+			label: "Title",
+			hiddenOnMobile: true
+		},
+		{
+			fieldName: "Phone",
+			label: "Phone",
+			type: "phone"
+		},
+		{
+			fieldName: "Email",
+			label: "E-Mail",
+			type: "email"
+		}
+	];
 
-    handleStudentSelected(event){
-        const studentId = event.detail.studentId;
-        this.updateSelectedStudent(studentId);
-        }
-    updateSelectedStudent(studentId){
-        let grid = this.template.querySelector('c-responsive-datatable');
-        let gallery = this.template.querySelector('c-student-tiles');
-        if (grid) {
-            grid.setSelectedRecord(studentId);
-            }
-        if (gallery) {
-            gallery.setSelectedStudent(studentId);
-            }
-        publish(this.messageContext, SELECTED_STUDENT_CHANNEL, {
-        studentId: studentId
-            });
-        }
-    handleRowDblClick(event) {
-            const studentId = event.detail.pk;
-            this[NavigationMixin.Navigate]({
-                type: 'standard__recordPage',
-                attributes: {
-                recordId: studentId,
-                objectApiName: 'Contact',
-                actionName: 'edit'
-               }
-            });
-        }
-    handleRowClick(event) {
-        let studentId = event.detail.pk;
-        this.updateSelectedStudent(studentId);
-        }    
+	handleFilterChange(event) {
+		this.selectedDeliveryId = event.detail.deliveryId;
+		this.selectedInstructorId = event.detail.instructorId;
+		this.dispatchEvent(new CustomEvent("loading", { bubbles: true, composed: true }));
+	}
+
+	handleStudentSelected(event) {
+		const studentId = event.detail.studentId;
+		this.updateSelectedStudent(studentId);
+	}
+	updateSelectedStudent(studentId) {
+		let grid = this.template.querySelector("c-responsive-datatable");
+		let gallery = this.template.querySelector("c-student-tiles");
+		if (grid) {
+			grid.setSelectedRecord(studentId);
+		}
+		if (gallery) {
+			gallery.setSelectedStudent(studentId);
+		}
+		publish(this.messageContext, SELECTED_STUDENT_CHANNEL, {
+			studentId: studentId
+		});
+	}
+	handleRowDblClick(event) {
+		const studentId = event.detail.pk;
+		this[NavigationMixin.Navigate]({
+			type: "standard__recordPage",
+			attributes: {
+				recordId: studentId,
+				objectApiName: "Contact",
+				actionName: "edit"
+			}
+		});
+	}
+	handleRowClick(event) {
+		let studentId = event.detail.pk;
+		this.updateSelectedStudent(studentId);
+	}
 }
