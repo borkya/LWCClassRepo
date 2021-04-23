9e57bb6fbb67dbee8f3731aadeffc0e501d0124f
Swapnil Borkar <swapnil.borkar@salesforce.com>
2021-04-23 11:25:46 -0400

    Exercise 6-4 List to Form

--- force-app/Exercises/main/default/lwc/tripReportFormAdvanced/tripReportFormAdvanced.js
+++ force-app/Exercises/main/default/lwc/tripReportFormAdvanced/tripReportFormAdvanced.js
@@ -134,6 +134,7 @@ export default class TripReportFormAdvanced extends LightningElement {
 					//TODO #7: after record creation, store the new ID of the trip report in our recordId property
 					this.recordId = tripReport.id;
 					Utils.showToast(this, "Success", "Trip Report Created", "success");
+                    this.returnToBrowseMode();
 				})
 				.catch((error) => {
 					let errors = reduceErrors(error);
@@ -148,6 +149,7 @@ export default class TripReportFormAdvanced extends LightningElement {
 			updateRecord(recordInput)
 				.then(() => {
 					Utils.showToast(this, "Success", "Trip report updated", "success");
+                    this.returnToBrowseMode();
 				})
 				.catch((error) => {
 					let errors = reduceErrors(error);
@@ -163,4 +165,12 @@ export default class TripReportFormAdvanced extends LightningElement {
 	onBlur() {
 		this.saveButtonDisabled = !this.validateFields();
 	}
+    returnToBrowseMode() {
+        const evt = new CustomEvent('tripreportmodechange', {
+        detail: {
+        mode: "browse"
+        },
+        });
+        this.dispatchEvent(evt);
+        }
 }
